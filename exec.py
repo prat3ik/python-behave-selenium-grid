85ed8f660324bbba6de2a80b21399f316332aadb
Pratik Patel <pratikquora@gmail.com>
2020-03-19 18:24:47 +0530

    Updated exec.py

--- exec.py
+++ exec.py
@@ -1,13 +1,22 @@
 import sys
 import os
-browsers = sys.argv
-if browsers.__len__() !=  0:
-    for browser in browsers[1:]:
-        f = open("context/testsettings.json", "w")
-        f.write(
-            '{"url": "https://www.google.com","browser": "'+browser+'","driver_timeout": "5"}')
-        f.close()
-        os.system('behave')
+
+args = sys.argv
+if args.__len__() !=  0:
+    if args[1] == '--single':
+        for browser in args[3:]:
+            f = open("context/testsettings.json", "w")
+            f.write(
+                '{"url": "https://www.google.com","browser": "' + browser + '","driver_timeout": "5"}')
+            f.close()
+            os.system('behave -n ' + args[2])
+
+    else:
+        for browser in args[1:]:
+            f = open("context/testsettings.json", "w")
+            f.write(
+                '{"url": "https://www.google.com","browser": "' + browser + '","driver_timeout": "5"}')
+            f.close()
+            os.system('behave')
 
 os.system('allure serve reports/')
-
